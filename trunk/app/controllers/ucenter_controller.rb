0730257101d351626a45adf2aa1814e411b03915
PSVR <pmq2001@gmail.com>
2012-08-19 15:56:16 +0800

    UCenter加密解密还是有问题

--- trunk/app/controllers/ucenter_controller.rb
+++ trunk/app/controllers/ucenter_controller.rb
@@ -1,10 +1,13 @@
 # coding: utf-8
 class UcenterController < ApplicationController
   def uc
+    get = post = []
     code = params[:code]
-    text = UCenter::Php.authcode(code,'DECODE',UCenter.getdef('UC_KEY'))
-    puts text
-    render text:"#{text}" and return
+    puts code
+    decoded = UCenter::Php.authcode(code,'DECODE',UCenter.getdef('UC_KEY'))
+    UCenter::Php.parse_str(decoded,get)
+    binding.pry
+    # render text:"#{text}" and return
   end
   
 end
