d8574464465d71f09dbe63bcc20d2aac23a0c7bd
PSVR <pmq2001@gmail.com>
2012-08-28 23:36:27 +0800

    线下艰难调试DZ

--- trunk/app/models/ktv/discuz_admin.rb
+++ trunk/app/models/ktv/discuz_admin.rb
@@ -6,9 +6,13 @@ module Ktv
     include Shared::MechanizeParty
     ADMIN_USER = 'psvr'
     ADMIN_PASS = 'jknlff8-pro-17m7755'
-    def start_mode(mode)
+    def start_mode(mode=nil)
       @mode = mode
-      @base_url = "http://#{@mode}.kejian.tv/simple"
+      if @mod.present?
+        @base_url = "http://#{@mode}.kejian.#{Rails.env.development? ? 'lvh.me' : 'tv' }/simple"
+      else
+        @base_url = "http://kejian.#{Rails.env.development? ? 'lvh.me' : 'tv' }/simple"
+      end
       # 依赖于forum.php显示登陆框框
       @login_page = @agent.get("#{@base_url}/forum.php")
       form = @login_page.form_with(:id=>'lsform')
@@ -28,15 +32,12 @@ module Ktv
 
     def self.orthodoxize_course_forums!
       Course.all.each do |item|
-        @page = @agent.get("#{@base_url}/admin.php?action=forums&operation=edit&anchor=threadtypes")
-        form = @page.forms.first
-        selectlist = form.field_with(:name=>'fid')
-        selectlist.value = item.fid
-        @page = form.submit
+        @page = @agent.get("#{@base_url}/admin.php?action=forums&operation=edit&fid=#{item.fid}")
         form = @page.forms.last
         form['threadtypesnew[status]']='1'
         form['threadtypesnew[required]']='1'
         form['threadtypesnew[listable]']='1'
+        form['detailsubmit']='提交'
       end
     end
   end
