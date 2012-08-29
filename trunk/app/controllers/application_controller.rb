eda738646a60a34e355ec55c09a3192a7e9fec0a
P.S.V.R <pmq2001@gmail.com>
2012-08-29 22:14:03 +0800

    set_table_name is deprecated

--- trunk/app/controllers/application_controller.rb
+++ trunk/app/controllers/application_controller.rb
@@ -58,17 +58,7 @@ class ApplicationController < ActionController::Base
     @is_ie10 = (agent.index('msie 10')!=nil)
   end
   def xookie
-    dz_auth = cookies[Discuz.cookiepre_real+'auth']
-    dz_saltkey = cookies[Discuz.cookiepre_real+'saltkey']
-    if current_user.nil? and dz_auth.present?
-      u = User.authenticate_through_dz_auth!(request,dz_auth,dz_saltkey)
-      if u
-        u.update_attribute("last_login_at",Time.now)
-        u.inc(:login_times,1)
-        LoginLog.create(:user_id=>u.id,:login_at=>Time.now,:range=>(Time.now.to_date-u.created_at.to_date).to_i)
-        sign_in(u)
-      end
-    end
+    Discuz::Utils.get_xookie.call
   end
   layout :layout_by_resource
   def layout_by_resource
