04da7065af83ac1e4ee10b11cfa2f3e06a93698d
P.S.V.R <pmq2001@gmail.com>
2012-09-05 14:42:03 +0800

    课程栏目合并，更好看的全部课程页面

--- trunk/app/models/department.rb
+++ trunk/app/models/department.rb
@@ -6,8 +6,19 @@ class Department
   include BaseModel
   field :name
   field :fid
+  field :courses_count,:type=>Integer,:default=>0
+  field :courses_count1,:type=>Integer,:default=>0
+  field :courses_count2,:type=>Integer,:default=>0
   validates_uniqueness_of :name,:message=>'与已有院系名重复，请尝试其他名'
   cache_consultant :fid,:from_what => :name,:no_callbacks=>true
+  def courses
+    Course.where(:department=>self.name)
+  end
+  def set_counter
+    self.update_attribute(:courses_count,courses.count)
+    self.update_attribute(:courses_count1,courses.where(:is_yjs.ne=>true).count)
+    self.update_attribute(:courses_count2,courses.where(:is_yjs=>true).count)
+  end
   def self.fid_fill!
     self.asc('created_at').each_with_index do |item,index|
       item.update_attribute(:fid,PreForumForum.find_by_name_and_type("#{item.name}",'group').fid)
